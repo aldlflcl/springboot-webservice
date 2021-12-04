c48ee5f28a343c867ff2f5ee67b81084085d5607
aldlflcl <wjddhksaos@gmail.com>
2021-12-04 22:01:45 +0900

    gcp oauth 설정파일 추가

--- src/main/java/com/aldlflcl/book/springboot/Application.java
+++ src/main/java/com/aldlflcl/book/springboot/Application.java
@@ -1,13 +1,33 @@
 package com.aldlflcl.book.springboot;
 
+import com.aldlflcl.book.springboot.domain.posts.Posts;
+import com.aldlflcl.book.springboot.domain.posts.PostsRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.annotation.PostConstruct;
+
 @EnableJpaAuditing
 @SpringBootApplication
+@RequiredArgsConstructor
 public class Application {
+
+    private final PostsRepository postsRepository;
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        Posts posts = Posts.builder()
+                .title("타이틀")
+                .author("작가")
+                .content("내용")
+                .build();
+
+        postsRepository.save(posts);
     }
 }
