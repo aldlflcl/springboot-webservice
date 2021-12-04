d8e0595331755c951b564c0d9e4f1ce10fb0928b
aldlflcl <wjddhksaos@gmail.com>
2021-12-04 20:20:38 +0900

    게시글 등록 추가

--- src/main/java/com/aldlflcl/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/aldlflcl/book/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.aldlflcl.book.springboot.service.posts;
 
 import com.aldlflcl.book.springboot.domain.posts.Posts;
 import com.aldlflcl.book.springboot.domain.posts.PostsRepository;
+import com.aldlflcl.book.springboot.web.dto.PostsListResponseDto;
 import com.aldlflcl.book.springboot.web.dto.PostsResponseDto;
 import com.aldlflcl.book.springboot.web.dto.PostsSaveRequestDto;
 import com.aldlflcl.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -34,7 +38,13 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> {
             return new IllegalArgumentException("해당 게시글이 없습니다. id=" + id);
         });
-
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
