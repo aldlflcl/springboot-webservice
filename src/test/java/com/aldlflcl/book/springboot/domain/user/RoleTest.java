6919253526eb8f842c82546a5fae21332d3da029
aldlflcl <wjddhksaos@gmail.com>
2021-12-04 23:26:39 +0900

    구글 oauth로그인 기능 추가

---
+++ src/test/java/com/aldlflcl/book/springboot/domain/user/RoleTest.java
@@ -0,0 +1,19 @@
+package com.aldlflcl.book.springboot.domain.user;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RoleTest {
+
+    @Test
+    public void enum확인() throws Exception {
+        //given
+        System.out.println(Role.USER.name());
+
+        //when
+        
+        //then
+      
+     }
+}
